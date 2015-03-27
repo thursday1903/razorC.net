41612a80aedc9f159cc89d6efe949fbfcb2ee7ad
thursday1903 <dovietanh258@gmail.com>
2015-03-27 07:44:17 +0700

    Forwarding Invoice
    Forwarding Invoice, Commons, FSC Enum,

---
+++ App_Code/Constants_.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Constants
+/// </summary>
+public class Constants_
+{
+    public const String DATECOMPARE_INVALID = "From Date must be smaller than To Date";
+    public const String UPDATE_SUCCESS = "Update success";
+    public const String INPUT_A_FLOAT_VALUE = "Input a float value";
+    public const String INPUT_A_DECIMAL_VALUE = "Input a decimal (8,2) value";    
+    public const String UPDATE_TRANSACTION_FAIL = "System corrupted. Check your system log";
+	public Constants_()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+       
+}
---
+++ App_Code/FSCEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for FSCEnum
+/// </summary>
+public class FSCEnum
+{
+    public enum FSCEnum_
+    { 
+        DOX = 1,
+        WPX = 0
+    }
+
+    public enum HandlingFeeType
+    {
+        Fixed = 0,
+        Transhipments = 1
+    }
+}
