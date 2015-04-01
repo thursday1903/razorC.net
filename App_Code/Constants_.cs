19925d38c64cc1e690de6e5a867a33722d015232
thursday1903 <dovietanh258@gmail.com>
2015-04-01 13:26:30 +0700

    Update0104
    Update0104

--- App_Code/Constants_.cs
+++ App_Code/Constants_.cs
@@ -11,13 +11,16 @@ public class Constants_
     public const String DATECOMPARE_INVALID = "From Date must be smaller than To Date";
     public const String UPDATE_SUCCESS = "Update success";
     public const String INPUT_A_FLOAT_VALUE = "Input a float value";
-    public const String INPUT_A_DECIMAL_VALUE = "Input a decimal (8,2) value";    
+    public const String INPUT_A_DECIMAL_VALUE = "Input a decimal (8,2) value";
     public const String UPDATE_TRANSACTION_FAIL = "System corrupted. Check your system log";
-	public Constants_()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
-       
+    public const String UPDATE_VALUE_EXISTED = "Update value existed, your update be Rejected";
+    public const String DELETE_CONSTRAINT_VALUE = "Delete constraint value, your request rejected";
+
+    public Constants_()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
 }
